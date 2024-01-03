db6c26fdb9395ab1e16364853455d8df0ac2a965
devallever <devallever@163.com>
2024-01-03 17:45:42 +0800

    UI: watch-围观app部分UI

--- watch/src/main/java/com/allever/compose/project/watch/WatchMainActivity.kt
+++ watch/src/main/java/com/allever/compose/project/watch/WatchMainActivity.kt
@@ -3,23 +3,292 @@ package com.allever.compose.project.watch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  *@Description
  *@author: zq
  *@date: 2024/1/3
  */
-class WatchMainActivity: ComponentActivity() {
+class WatchMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //纵向布局
+            Column {
+                Content()
+                NavBar()
+            }
+        }
+    }
 
+    @Composable
+    fun ColumnScope.Content() {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color(0xfffafafa))
+        ) {
+            TopBar()
+            SearchBar()
+            TabBar()
+            LoveArea()
+        }
+    }
+
+
+    @Composable
+    fun RowScope.NavItem(@DrawableRes iconId: Int, tint: Color) {
+        Button(
+            onClick = { },
+            Modifier
+                .weight(1f)
+                .fillMaxHeight(), shape = RectangleShape,
+            colors = ButtonDefaults.outlinedButtonColors()
+        ) {
+            Icon(
+                painterResource(iconId), "图标", Modifier
+                    .size(24.dp), tint = tint
+            )
+        }
+    }
+
+    @Preview
+    @Composable()
+    fun NavBar() {
+        Row(
+            Modifier
+                .height(64.dp)
+                .padding(0.dp, 0.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            NavItem(iconId = R.drawable.input_bar_add, tint = Color(0xfffa9e51))
+            NavItem(iconId = R.drawable.input_bar_add, tint = Color(0xffb4b4b4))
+            NavItem(iconId = R.drawable.input_bar_add, tint = Color(0xffb4b4b4))
+            NavItem(iconId = R.drawable.input_bar_add, tint = Color(0xffb4b4b4))
+        }
+    }
+
+    @Preview
+    @Composable
+    fun TopBar() {
+//横向
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(28.dp, 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painterResource(id = R.drawable.ic_header),
+                "头像",
+                Modifier
+                    .size(72.dp)
+                    .clip(CircleShape)
+            )
+
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(10.dp)
+            ) {
+                Text(text = "欢迎回来!", fontSize = 14.sp, color = Color(0xffb4b4b4))
+                Text(text = "Allever", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            }
+
+            Surface(Modifier.clip(CircleShape), color = Color(0xfffef7f0)) {
+                Icon(
+                    painterResource(id = R.drawable.ic_notification),
+                    "通知",
+                    Modifier
+                        .size(64.dp)
+                        .padding(10.dp)
+                )
+            }
+
+        }
+    }
+
+    @Composable
+    fun SearchBar() {
+        Row(
+            Modifier
+                .padding(28.dp, 6.dp)
+                .clip(CircleShape)
+                .fillMaxWidth()
+                .height(56.dp)
+                .background(color = Color.White),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            var searchText by remember { mutableStateOf("") }
+            BasicTextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                Modifier
+                    .padding(start = 16.dp)
+                    .weight(1f), textStyle = TextStyle(fontSize = 16.sp)
+            ) {
+                if (searchText.isEmpty()) {
+                    Text(text = "搜搜看?", color = Color(0xffb4b4b4), fontSize = 16.sp)
+                }
+                it()
+            }
+
+            Box(
+                Modifier
+                    .padding(6.dp)
+                    .clip(CircleShape)
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+                    .background(color = Color(0xfffa9e51)),
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.ic_search),
+                    "搜索",
+                    Modifier
+                        .size(24.dp)
+                        .align(Alignment.Center), tint = Color.White
+                )
+            }
+
+        }
+    }
+
+    @Composable
+    fun TabBar() {
+        val tabList = listOf("推荐", "萌宠", "科技", "影院", "时事", "热点", "人文", "体育", "少儿")
+        var selected by remember { mutableStateOf(0) }
+        LazyRow(
+            Modifier
+                .padding(4.dp, 8.dp)
+                .height(44.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            contentPadding = PaddingValues(12.dp, 0.dp)
+        ) {
+            itemsIndexed(tabList) { index, item ->
+                Column(
+                    Modifier
+                        .padding(16.dp, 4.dp)
+                        .width(IntrinsicSize.Max)
+                ) {
+                    Text(
+                        fontWeight = if (index == selected) FontWeight.Bold else FontWeight.Normal,
+                        text = item,
+                        color = if (index == selected) Color(0xfffa9e51) else Color(0xffb4b4b4)
+                    )
+
+                    if (index == selected) {
+                        Box(
+                            Modifier
+                                .fillMaxWidth()
+                                .height(2.dp)
+                                .clip(RoundedCornerShape(1.dp))
+                                .background(color = Color(0xfffa9e51))
+                        ) {
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    @Composable
+    fun LoveArea() {
+        Column {
+            Row(Modifier.padding(28.dp, 8.dp)) {
+                Text(text = "TA 爱的", fontWeight = FontWeight.Bold)
+                Spacer(Modifier.weight(1f))
+                Text(text = "查看全部", color = Color(0xffb4b4b4))
+            }
+
+
+            val loveItemList = mutableListOf<LoveItem>().apply {
+                for (i in 0..10) {
+                    add(LoveItem(R.drawable.ic_header, "Title", "Desc", i.toFloat()))
+                }
+            }
+
+            LazyRow {
+                itemsIndexed(loveItemList) { index, item ->
+                    Column(
+                        Modifier
+                            .padding(8.dp, 0.dp)
+                            .width(200.dp)
+                            .height(300.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(color = Color.White)
+                    ) {
+                        Image(
+                            painterResource(id = item.cover),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .clip(
+                                    RoundedCornerShape(8.dp)
+                                )
+                                .fillMaxWidth()
+                                .height(200.dp)
+                        )
+
+                        Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
+                            Column(Modifier.weight(1f)) {
+
+                            }
+
+                            Row(
+                                Modifier
+                                    .background(color = Color(0xfffef7f0))
+                                    .padding(10.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    painterResource(id = R.drawable.ic_star),
+                                    contentDescription = "score",
+                                    Modifier.size(24.dp),
+                                    tint = Color(0xfffa9e51)
+                                )
+
+                                Text(
+                                    text = item.score.toString(),
+                                    fontSize = 16.sp,
+                                    modifier = Modifier.padding(6.dp, 0.dp),
+                                    color = Color(0xfffa9e51)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-@Composable()
-fun init() {
+data class LoveItem(val cover: Int, val title: String, val desc: String, val score: Float)
 
-}
+
+
+
