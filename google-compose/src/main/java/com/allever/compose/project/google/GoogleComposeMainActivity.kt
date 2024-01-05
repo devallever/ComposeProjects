ac1f0b926f978659b9bef7e2db8539668a89072c
devallever <devallever@163.com>
2024-01-05 17:52:23 +0800

    FEATURE: google-compose 教程,11.动画

--- google-compose/src/main/java/com/allever/compose/project/google/GoogleComposeMainActivity.kt
+++ google-compose/src/main/java/com/allever/compose/project/google/GoogleComposeMainActivity.kt
@@ -3,16 +3,20 @@ package com.allever.compose.project.google
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.allever.compose.project.google.ui.theme.GoogleComposeProjectsTheme
 
@@ -39,7 +43,13 @@ fun Greeting(name: String) {
     val expended = rememberSaveable {
         mutableStateOf(false)
     }
-    val expendedPadding = if (expended.value) 48.dp else 0.dp
+    val expendedPadding by animateDpAsState(
+        targetValue = if (expended.value) 48.dp else 0.dp,
+        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         modifier = Modifier
@@ -54,7 +64,9 @@ fun Greeting(name: String) {
                     .padding(bottom = expendedPadding)
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             ElevatedButton(onClick = {
                 expended.value = !expended.value
@@ -94,7 +106,7 @@ fun MyApp(
         }
     } else {
         //可以用 Surface 包围 可组合项, 设置背景颜色
-        val names = List(30) { "${it+1}" }
+        val names = List(30) { "${it + 1}" }
         LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
             items(names) {
                 Greeting(name = it)
