42cdc804e480de44106537321db8141ce9456934
devallever <devallever@163.com>
2024-03-16 21:26:05 +0800

    FEATURE: 基础控件

--- compose-basic/src/main/java/com/allever/compose/project/compose/basic/BasicWidgetActivity.kt
+++ compose-basic/src/main/java/com/allever/compose/project/compose/basic/BasicWidgetActivity.kt
@@ -11,13 +11,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,10 +20,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.lifecycleScope
 import coil.compose.AsyncImage
 import com.allever.compose.core.toast
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  *@Description
@@ -36,10 +36,134 @@ import com.allever.compose.core.toast
  *@date: 2024/3/15
  */
 class BasicWidgetActivity : ComponentActivity() {
+
+    @Composable
+    fun AlertDialogExample(
+        onDismissRequest: () -> Unit,
+        onConfirmation: () -> Unit,
+        dialogTitle: String,
+        dialogText: String,
+        icon: Int,
+    ) {
+        AlertDialog(
+            icon = {
+                Icon(
+                    painterResource(id = icon),
+                    contentDescription = "Example Icon",
+                    modifier = Modifier
+                        .size(42.dp)
+                        .padding(10.dp)
+                )
+            },
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                Text(text = dialogText)
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text("Dismiss")
+                }
+            }
+        )
+    }
+
+    @Composable
+    fun CustomDialogView(
+        onDismissRequest: () -> Unit,
+        onConfirmation: () -> Unit,
+    ) {
+        Dialog(onDismissRequest = onDismissRequest) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "CustomDialogView",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        textAlign = TextAlign.Center,
+                        color = Color.Black
+                    )
+
+                    Text(
+                        text = "CustomDialogView Message",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        textAlign = TextAlign.Center,
+                        color = Color.Gray
+                    )
+
+                    Row() {
+                        TextButton(onClick = {
+                            onConfirmation.invoke()
+                        }) {
+                            Text(text = "Confirm")
+                        }
+
+                        TextButton(onClick = {
+                            onDismissRequest.invoke()
+                        }) {
+                            Text(text = "Dismiss")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+
+            var showAlertDialog by remember {
+                mutableStateOf(false)
+            }
+            if (showAlertDialog) {
+                AlertDialogExample(
+                    onDismissRequest = { showAlertDialog = false },
+                    onConfirmation = { showAlertDialog = false },
+                    dialogTitle = "AlertDialogExample",
+                    dialogText = "This is AlertDialogExample",
+                    icon = R.drawable.ic_menu
+                )
+            }
+
+            var customDialog by remember {
+                mutableStateOf(false)
+            }
+            if (customDialog) {
+                CustomDialogView(onDismissRequest = {
+                    customDialog = false
+                }, onConfirmation = {
+                    customDialog = false
+                })
+            }
 
             //线性布局, 占满全屏
             Column(
@@ -53,6 +177,26 @@ class BasicWidgetActivity : ComponentActivity() {
             ) {
 
                 Text(text = "滚动布局 verticalScroll", Modifier.padding(10.dp))
+
+                Text(text = "垂直布局 Column", Modifier.padding(10.dp))
+
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .background(Color(0x80ff0000))
+                ) {
+                }
+
+                Text(text = "水平布局 Row", Modifier.padding(10.dp))
+
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .background(Color(0x8000ff00))
+                ) {
+                }
 
                 Text(
                     text = "文本用 Text",
@@ -120,6 +264,69 @@ class BasicWidgetActivity : ComponentActivity() {
                     Text(text = "Click Me")
                 }
 
+                Text(text = "实心按钮 Button", Modifier.padding(10.dp))
+                Button(onClick = { toast("Button") }) {
+                    Text(text = "Button")
+                }
+
+                Text(text = "颜色填充按钮 FilledTonalButton", Modifier.padding(10.dp))
+                FilledTonalButton(onClick = { toast("FilledTonalButton") }) {
+                    Text(text = "FilledTonalButton")
+                }
+
+                Text(text = "轮廓按钮 OutlinedButton", Modifier.padding(10.dp))
+                OutlinedButton(onClick = { toast("OutlinedButton") }) {
+                    Text(text = "OutlinedButton")
+                }
+
+                Text(text = "阴影按钮 ElevatedButton", Modifier.padding(10.dp))
+                ElevatedButton(onClick = { toast("ElevatedButton") }) {
+                    Text(text = "ElevatedButton")
+                }
+
+                Text(text = "文本按钮 TextButton", Modifier.padding(10.dp))
+                TextButton(onClick = { toast("TextButton") }) {
+                    Text(text = "TextButton")
+                }
+
+                Text(text = "图标按钮 IconButton", Modifier.padding(10.dp))
+                IconButton(onClick = { toast("IconButton") }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_menu),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(42.dp)
+                            .padding(12.dp)
+                    )
+                }
+
+                Text(text = "悬浮按钮 FloatingActionButton", Modifier.padding(10.dp))
+                FloatingActionButton(onClick = {
+                    toast("FloatingActionButton")
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_menu),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(42.dp)
+                            .padding(12.dp)
+                    )
+                }
+
+                Text(text = "扩展悬浮按钮 ExtendedFloatingActionButton", Modifier.padding(10.dp))
+                ExtendedFloatingActionButton(onClick = {
+                    toast("ExtendedFloatingActionButton")
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_menu),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(42.dp)
+                            .padding(12.dp)
+                    )
+                    Text(text = "ExtendedFloatingActionButton")
+                }
+
                 Text(text = "输入框 BasicTextField", Modifier.padding(10.dp))
 
                 var content by remember { mutableStateOf("") }
@@ -173,22 +380,199 @@ class BasicWidgetActivity : ComponentActivity() {
                     Text(text = "Confirm")
                 }
 
+                Text(text = "选中框 CheckBox", Modifier.padding(10.dp))
+
+                var checkBoxChecked by remember { mutableStateOf(false) }
+                Checkbox(checked = checkBoxChecked, onCheckedChange = {
+                    toast("is Checked = $it")
+                    checkBoxChecked = it
+                })
+
+                Text(text = "单选 组合RadioButton", Modifier.padding(10.dp))
+                val radioButtonText = listOf("A", "B", "C", "D")
+                var radioButtonTag by remember {
+                    mutableStateOf("")
+                }
+                Row(
+                    modifier = Modifier.padding(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    radioButtonText.forEach {
+                        RadioButton(selected = it == radioButtonTag, onClick = {
+                            toast(it)
+                            radioButtonTag = it
+                        })
+
+                        Text(text = it)
+                    }
+                }
+
+                Text(text = "开关 Switch", Modifier.padding(10.dp))
+
+                var switchCheck by remember {
+                    mutableStateOf(false)
+                }
+                Switch(checked = switchCheck, onCheckedChange = {
+                    switchCheck = it
+                })
 
 
+                Text(text = "拖动进度条 Slide", Modifier.padding(10.dp))
+                var slideValue by remember {
+                    mutableStateOf(0F)
+                }
+                Slider(
+                    valueRange = 0f..100f,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    value = slideValue,
+                    onValueChange = {
+                        slideValue = it
+                    },
+                    onValueChangeFinished = {
+                        toast("slideValue: ${slideValue.toInt()}")
+                    })
+
+                Text(text = "日期选择器 DatePicker", Modifier.padding(10.dp))
+                val datePickerState = rememberDatePickerState()
+                DatePicker(state = datePickerState)
+                Text(
+                    "当前选中日期的时间戳 ${datePickerState.selectedDateMillis ?: "没有选择"}",
+                    modifier = Modifier.padding(10.dp)
+                )
+
+                Text(text = "时间选择器 TimePicker", Modifier.padding(10.dp))
+                val timePickerState = rememberTimePickerState()
+                TimePicker(state = timePickerState)
+                Text(
+                    "当前选中的时间 ${timePickerState.hour}:${timePickerState.minute}",
+                    modifier = Modifier.padding(10.dp)
+                )
+
+                Text(text = "卡片 Card", Modifier.padding(10.dp))
+
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 4.dp,
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .padding(horizontal = 16.dp)
+                ) {
+
+                }
 
 
+                Text(text = "顶部状态栏 Card", Modifier.padding(10.dp))
 
+                TopAppBar(
+                    title = {
+                        Text(text = "TopAppBar", fontSize = 16.sp)
+                    }, modifier = Modifier, colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color(0x80333333)
+                    ), navigationIcon = {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_menu),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(42.dp)
+                                .padding(12.dp)
+                        )
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            toast("menu1")
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_menu),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(42.dp)
+                                    .padding(12.dp)
+                            )
+                        }
+                        IconButton(onClick = {
+                            toast("menu2")
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_menu),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(42.dp)
+                                    .padding(12.dp)
+                            )
+                        }
+                    }
+                )
+
+                Text(text = "系统默认弹窗 AlertDialog", Modifier.padding(10.dp))
+                Button(onClick = {
+                    showAlertDialog = true
+                }) {
+                    Text(text = "AlertDialog")
+                }
+
+                Text(text = "自定义弹窗 Dialog", Modifier.padding(10.dp))
+                Button(onClick = {
+                    customDialog = true
+                }) {
+                    Text(text = "Dialog")
+                }
+
+
+                Text(text = "圆形进度 CircularProgressIndicator", Modifier.padding(10.dp))
+                CircularProgressIndicator()
+
+                var circleProgressValue by remember {
+                    mutableStateOf(0.3f)
+                }
+                Text(text = "带进度的圆形进度 CircularProgressIndicator", Modifier.padding(10.dp))
+                CircularProgressIndicator(progress = circleProgressValue)
+                Button(onClick = {
+                    lifecycleScope.launch {
+                        for (i in 0..100) {
+                            circleProgressValue = i.toFloat() / 100
+                            delay(10)
+                        }
+                    }
+                }, modifier = Modifier.padding(10.dp)) {
+                    Text(text = "启动进度")
+                }
+
+                Text(text = "水平进度 LinearProgressIndicator", Modifier.padding(10.dp))
+                LinearProgressIndicator()
+
+                var linearProgressValue by remember {
+                    mutableStateOf(0.3f)
+                }
+                Text(text = "带进度的水平进度 LinearProgressIndicator", Modifier.padding(10.dp))
+                LinearProgressIndicator(progress = linearProgressValue)
+                Button(onClick = {
+                    lifecycleScope.launch {
+                        for (i in 0..100) {
+                            linearProgressValue = i.toFloat() / 100
+                            delay(10)
+                        }
+                    }
+                }, modifier = Modifier.padding(10.dp)) {
+                    Text(text = "启动进度")
+                }
 
 
 
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(20.dp)
+                        .height(40.dp)
                 )
 
 
             }
         }
     }
+
+
 }
