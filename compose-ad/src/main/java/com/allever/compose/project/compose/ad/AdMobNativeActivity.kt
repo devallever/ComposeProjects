5b1cee411bc04fb49e491b3a25881c4c8aa05868
devallever <devallever@163.com>
2024-03-25 21:04:38 +0800

    FEATURE: 优化原生banner

--- compose-ad/src/main/java/com/allever/compose/project/compose/ad/AdMobNativeActivity.kt
+++ compose-ad/src/main/java/com/allever/compose/project/compose/ad/AdMobNativeActivity.kt
@@ -16,13 +16,17 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.core.view.isVisible
+import com.allever.compose.core.log
 import com.allever.compose.core.ui.ComposeProjectTheme
 import com.allever.compose.project.compose.ad.databinding.NativeAdSmallBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 
 class AdMobNativeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,7 +62,8 @@ class AdMobNativeActivity : ComponentActivity() {
                 return@AndroidViewBinding
             }
 
-            val adView = adNativeView.also { adNativeView ->
+            adNativeView.isVisible = false
+            adNativeView.also { adNativeView ->
 //                adView.adChoicesView = adChoice
                 adNativeView.bodyView = adBody
                 adNativeView.iconView = adIcon
@@ -67,34 +72,36 @@ class AdMobNativeActivity : ComponentActivity() {
                 adNativeView.mediaView = adMedia
                 adNativeView.storeView = adStore
                 adNativeView.priceView = adPrice
-//                adBody.text = nativeAd.body
-//                adHeadline.text = nativeAd.headline
-//                Glide.with(adIcon!!).load(nativeAd.icon?.drawable)
-//                    .transform(CenterInside(), RoundedCorners(8)).into(adIcon)
-//                adCta?.text = nativeAd.callToAction
-//                nativeAd.mediaContent?.let {
-//                    adMedia?.setMediaContent(it)
-//                }
-//                adStore?.text = nativeAd.store
-//                adPrice?.text = nativeAd.price
-//                adNativeView.setNativeAd(nativeAd)
-//                adViewContainer.addView(adNativeView)
 
                 kotlin.runCatching {
                     AdLoader.Builder(this@AdMobNativeActivity, adId)
                         .forNativeAd { nativeAd ->
                             adBody.text = nativeAd.body
                             adHeadline.text = nativeAd.headline
-                            Glide.with(adIcon!!).load(nativeAd.icon?.drawable)
+                            Glide.with(adIcon).load(nativeAd.icon?.drawable)
                                 .transform(CenterInside(), RoundedCorners(8)).into(adIcon)
                             adCta.text = nativeAd.callToAction
                             nativeAd.mediaContent?.let {
-                                adMedia?.setMediaContent(it)
+                                adMedia.mediaContent = it
                             }
-                            adStore?.text = nativeAd.store
-                            adPrice?.text = nativeAd.price
+                            adStore.text = nativeAd.store
+                            adPrice.text = nativeAd.price
                             adNativeView.setNativeAd(nativeAd)
                         }
+                        .withAdListener(object : AdListener() {
+                            override fun onAdLoaded() {
+                                log("small native ad loaded")
+                                adNativeView.isVisible = true
+                            }
+
+                            override fun onAdFailedToLoad(error: LoadAdError) {
+                                log("small native ad fail to load: ${error.message}")
+                            }
+
+                            override fun onAdClosed() {
+
+                            }
+                        })
                 }
                     .onSuccess {
                         it.build().loadAd(AdRequest.Builder().build())
