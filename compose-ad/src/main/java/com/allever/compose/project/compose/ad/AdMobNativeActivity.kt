2de70f9b4dd2a9e3727575cb8ebaf7dec4b48cd0
devallever <devallever@163.com>
2024-03-25 22:00:30 +0800

    FEATURE: 列表原生banner

--- compose-ad/src/main/java/com/allever/compose/project/compose/ad/AdMobNativeActivity.kt
+++ compose-ad/src/main/java/com/allever/compose/project/compose/ad/AdMobNativeActivity.kt
@@ -3,15 +3,23 @@ package com.allever.compose.project.compose.ad
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,8 +44,7 @@ class AdMobNativeActivity : ComponentActivity() {
             ComposeProjectTheme {
                 Column(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .verticalScroll(rememberScrollState()),
+                        .fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
@@ -55,6 +62,40 @@ class AdMobNativeActivity : ComponentActivity() {
                     )
 
                     BigNativeAd(Modifier.padding(horizontal = 10.dp))
+
+                    Text(
+                        text = "列表 Banner广告",
+                        Modifier.padding(10.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    val list = mutableListOf<Product>().apply {
+                        add(Product("Banana"))
+                        add(Product("", true))
+                        add(Product("Orange"))
+                    }
+                    LazyColumn {
+                        itemsIndexed(list) { index, item ->
+                            if (item.isAd) {
+                                SmallNativeAd()
+                            } else {
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(48.dp)
+                                        .background(Color(0x80ff0000))
+                                    , horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(
+                                        text = item.name,
+                                        modifier = Modifier
+                                    )
+                                }
+
+                            }
+                        }
+                    }
 
                 }
             }
@@ -177,5 +218,7 @@ class AdMobNativeActivity : ComponentActivity() {
         }
 
     }
+
+    data class Product(val name: String, val isAd: Boolean = false)
 }
 
