3688b4aca9e8fc3fe205d2010726fe8939b73853
devallever <devallever@163.com>
2024-03-25 21:25:50 +0800

    FEATURE: 原生大banner

--- compose-ad/src/main/java/com/allever/compose/project/compose/ad/AdMobNativeActivity.kt
+++ compose-ad/src/main/java/com/allever/compose/project/compose/ad/AdMobNativeActivity.kt
@@ -19,7 +19,8 @@ import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.core.view.isVisible
 import com.allever.compose.core.log
 import com.allever.compose.core.ui.ComposeProjectTheme
-import com.allever.compose.project.compose.ad.databinding.NativeAdSmallBinding
+import com.allever.compose.project.compose.ad.databinding.NativeAdBannerBigBinding
+import com.allever.compose.project.compose.ad.databinding.NativeAdBannerSmallBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -45,7 +46,15 @@ class AdMobNativeActivity : ComponentActivity() {
                         fontWeight = FontWeight.Bold
                     )
 
-                    SmallNativeAd()
+                    SmallNativeAd(Modifier.padding(horizontal = 10.dp))
+
+                    Text(
+                        text = "原生 Banner广告 Big",
+                        Modifier.padding(10.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    BigNativeAd(Modifier.padding(horizontal = 10.dp))
 
                 }
             }
@@ -53,18 +62,75 @@ class AdMobNativeActivity : ComponentActivity() {
     }
 
     @Composable
-    fun SmallNativeAd() {
+    fun BigNativeAd(modifier: Modifier = Modifier) {
         val context = LocalContext.current
         val adId = Constants.NATIVE_TEST_ID
         var requested = false
-        AndroidViewBinding(factory = NativeAdSmallBinding::inflate) {
+        AndroidViewBinding(factory = NativeAdBannerBigBinding::inflate, modifier = modifier) {
             if (requested) {
                 return@AndroidViewBinding
             }
 
             adNativeView.isVisible = false
             adNativeView.also { adNativeView ->
-//                adView.adChoicesView = adChoice
+                adNativeView.bodyView = adBody
+                adNativeView.iconView = adIcon
+                adNativeView.headlineView = adHeadline
+                adNativeView.callToActionView = adCta
+                adNativeView.mediaView = adMedia
+                adNativeView.storeView = adStore
+                adNativeView.priceView = adPrice
+
+                kotlin.runCatching {
+                    AdLoader.Builder(this@AdMobNativeActivity, adId)
+                        .forNativeAd { nativeAd ->
+                            adBody.text = nativeAd.body
+                            adHeadline.text = nativeAd.headline
+                            Glide.with(adIcon).load(nativeAd.icon?.drawable)
+                                .transform(CenterInside(), RoundedCorners(8)).into(adIcon)
+                            adCta.text = nativeAd.callToAction
+                            nativeAd.mediaContent?.let {
+                                adMedia.mediaContent = it
+                            }
+                            adStore.text = nativeAd.store
+                            adPrice.text = nativeAd.price
+                            adNativeView.setNativeAd(nativeAd)
+                        }
+                        .withAdListener(object : AdListener() {
+                            override fun onAdLoaded() {
+                                log("big native ad loaded")
+                                adNativeView.isVisible = true
+                            }
+
+                            override fun onAdFailedToLoad(error: LoadAdError) {
+                                log("big native ad fail to load: ${error.message}")
+                            }
+
+                            override fun onAdClosed() {
+
+                            }
+                        })
+                }
+                    .onSuccess {
+                        it.build().loadAd(AdRequest.Builder().build())
+                        requested = true
+                    }
+            }
+        }
+    }
+
+    @Composable
+    fun SmallNativeAd(modifier: Modifier = Modifier) {
+        val context = LocalContext.current
+        val adId = Constants.NATIVE_TEST_ID
+        var requested = false
+        AndroidViewBinding(factory = NativeAdBannerSmallBinding::inflate, modifier = modifier) {
+            if (requested) {
+                return@AndroidViewBinding
+            }
+
+            adNativeView.isVisible = false
+            adNativeView.also { adNativeView ->
                 adNativeView.bodyView = adBody
                 adNativeView.iconView = adIcon
                 adNativeView.headlineView = adHeadline
